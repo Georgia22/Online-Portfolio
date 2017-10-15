b8303e7b53996f08600e3be647484a00f108294a
Alina <alina.baicu93@yahoo.com>
2017-10-15 18:46:09 +0000

    Online Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* global $ */
+$(document).ready(function(){
+        $(function(){
+        $(".typed").typed({
+        strings: ["&#60;&#62;Aspiring Front End Web Developer&#60;&#47;&#62;"],
+        typeSpeed: 40,
+    });
+
+    });
+});
